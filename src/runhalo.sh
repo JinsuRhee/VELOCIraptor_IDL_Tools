ee1a35f56118ea3ae91fc496352f55082fe85d94
JinsuRhee <jinsu.rhee@gmail.com>
2021-09-03 10:10:18 +0900

    updated

--- src/runhalo.sh
+++ src/runhalo.sh
@@ -1,5 +1,5 @@
-for i in $(seq -f "%04g" 0243 0243)
+for i in $(seq -f "%04g" 0111 0170)
 do
-	sed -i "99c P_VRrun_snap = [${i}, ${i}, 1L]" ../settings.nml
+	sed -i "154c P_VRrun_snap = [${i}, ${i}, 1L]" ../settings.nml
 	idl -e main
 done
